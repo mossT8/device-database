fecd1258bc7655e0b3f3b6b4c6deb8bc6f64d2d0
mossT8 <tannar.moss@gmail.com>
2024-10-26 11:29:00 +0200

    Check In
    updated created table and seed_env and seed_system_defaults scripts to reflect most recent code changes

--- scripts/seed_system_defaults.sql
+++ scripts/seed_system_defaults.sql
@@ -1,53 +1,39 @@
 -- Insert more data into the units table
 INSERT INTO units (name, symbol)
 VALUES ('None', '');
-SET
-    @NoneUnitsId = last_insert_id();
+SET @NoneUnitsId = last_insert_id();
 INSERT INTO units (name, symbol)
 VALUES ('Percentage', '%');
-SET
-    @PercentageUnitsId = last_insert_id();
+SET @PercentageUnitsId = last_insert_id();
 INSERT INTO units (name, symbol)
 VALUES ('Celsius', '°C');
-SET
-    @CelsiusUnitsId = last_insert_id();
+SET @CelsiusUnitsId = last_insert_id();
 INSERT INTO units (name, symbol)
 VALUES ('Fahrenheit', '°F');
-SET
-    @FahrenheitUnitsId = last_insert_id();
+SET @FahrenheitUnitsId = last_insert_id();
 INSERT INTO units (name, symbol)
 VALUES ('Kelvin', 'K');
-SET
-    @KelvinUnitsId = last_insert_id();
+SET @KelvinUnitsId = last_insert_id();
 INSERT INTO units (name, symbol)
 VALUES ('Liters', 'L');
-SET
-    @LitersUnitsId = last_insert_id();
+SET @LitersUnitsId = last_insert_id();
 INSERT INTO units (name, symbol)
 VALUES ('Milliliters', 'mL');
-SET
-    @MillilitersUnitsId = last_insert_id();
+SET @MillilitersUnitsId = last_insert_id();
 INSERT INTO units (name, symbol)
 VALUES ('Lumens', 'lm');
-SET
-    @LumensUnitsId = last_insert_id();
-
--- Insert data into the sensor table
-INSERT INTO sensor (code, unit_id, name)
-VALUES ('S00001', @PercentageUnitsId, 'Soil Sensor');
-SET
-    @SoliSensorId = last_insert_id();
-INSERT INTO sensor (code, unit_id, name)
-VALUES ('T00001',@CelsiusUnitsId, 'Temparture Sensor');
-SET
-    @TemperatureSensor = last_insert_id();
-INSERT INTO sensor (code, unit_id, name)
-VALUES ('L00001',@LumensUnitsId, 'Light Sensor');
-SET
-    @LightSensor = last_insert_id();
-
+SET @LumensUnitsId = last_insert_id();
+-- Insert data into the sensors table
+INSERT INTO sensors (code, unit_id, name, config_required, config_default)
+VALUES ('S00001', @PercentageUnitsId, 'Soil sensors', JSON_OBJECT('min', 0, 'max', 100), JSON_OBJECT('min', 0, 'max', 100));
+SET @SolisensorsId = last_insert_id();
+INSERT INTO sensors (code, unit_id, name,  config_required, config_default)
+VALUES ('T00001', @CelsiusUnitsId, 'Temparture sensors', JSON_OBJECT('min', -50, 'max', 50), JSON_OBJECT('min', -50, 'max', 50));
+SET @Temperaturesensors = last_insert_id();
+INSERT INTO sensors (code, unit_id, name, config_required, config_default)
+VALUES ('L00001', @LumensUnitsId, 'Light sensors', JSON_OBJECT('min', 0, 'max', 1000), JSON_OBJECT('min', 0, 'max', 1000));
+SET @Lightsensors = last_insert_id();
 -- Insert data into the models table
 INSERT INTO models (code, name)
 VALUES ('CHIP0001', 'ESP32');
-SET
-    @ESP32ModelId = last_insert_id();
+SET @ESP32ModelId = last_insert_id();
